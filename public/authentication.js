4bb5b6c4c5bc9ddd030215c50d1354a0d448af4f
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-01-24 19:01:52 +0530

    Added required option for userInput.

--- public/authentication.js
+++ public/authentication.js
@@ -7,13 +7,13 @@ const getAuthDiv = function(document, divType, otherType) {
 
 const divs = {
   login: `<form method="POST" action="/login">
-  <p class="field">Username : <input class="field" name="userName" type="text"/></p>
-  <p class="field">Password : <input class="field" name="password" type="password"/></p>
+  <p class="field">Username : <input class="field" name="userName" type="text" required/></p>
+  <p class="field">Password : <input class="field" name="password" type="password" required/></p>
   <input class="field" type="submit" value="Confirm"/>
   </form>`,
   signup: `<form method="POST" action="/signup">
-  <p class="field">Username : <input class="field" name="userName" type="text"/></p>
-  <p class="field">Password : <input class="field" name="password" type="password"/></p>
+  <p class="field">Username : <input class="field" name="userName" type="text" required/></p>
+  <p class="field">Password : <input class="field" name="password" type="password" required/></p>
   <input class="field" type="submit" value="Create Account"/>
   </form>`
 };
