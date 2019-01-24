d08b52980bdaf243b66733cf429fdb544255ee33
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-01-24 18:57:54 +0530

    Handling difference between login and signup in the homepage.

--- public/authentication.js
+++ public/authentication.js
@@ -1,26 +1,28 @@
-const getAuthDiv = function(document, divType) {
+const getAuthDiv = function(document, divType, otherType) {
   const authDiv = document.getElementById('authDiv');
+  document.getElementById(divType).style.color = 'red';
+  document.getElementById(otherType).style.color = 'black';
   authDiv.innerHTML = divs[divType];
 };
 
 const divs = {
-  loginDiv: `<form method="POST" action="/login">
+  login: `<form method="POST" action="/login">
   <p class="field">Username : <input class="field" name="userName" type="text"/></p>
   <p class="field">Password : <input class="field" name="password" type="password"/></p>
-  <input class="field" type="submit"/>
+  <input class="field" type="submit" value="Confirm"/>
   </form>`,
-  signupDiv: `<form method="POST" action="/signup">
+  signup: `<form method="POST" action="/signup">
   <p class="field">Username : <input class="field" name="userName" type="text"/></p>
   <p class="field">Password : <input class="field" name="password" type="password"/></p>
-  <input class="field" type="submit"/>
+  <input class="field" type="submit" value="Create Account"/>
   </form>`
 };
 
 const initialize = function() {
   const logiButton = document.getElementById('login');
-  logiButton.onclick = getAuthDiv.bind(null, document, 'loginDiv');
+  logiButton.onclick = getAuthDiv.bind(null, document, 'login', 'signup');
   const signupButton = document.getElementById('signup');
-  signupButton.onclick = getAuthDiv.bind(null, document, 'signupDiv');
+  signupButton.onclick = getAuthDiv.bind(null, document, 'signup', 'login');
 };
 
 window.onload = initialize;
